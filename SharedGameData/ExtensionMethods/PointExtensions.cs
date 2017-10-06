9506bbcf74764d88813de0f1968049ee1d2d176a
NateBowman <NateBowman@users.noreply.github.com>
2017-10-06 15:06:22 +0100

    Added Point/RectExtensions

---
+++ SharedGameData/ExtensionMethods/PointExtensions.cs
@@ -0,0 +1,22 @@
+﻿#region
+
+using Microsoft.Xna.Framework;
+using Point = System.Drawing.Point;
+
+#endregion
+
+namespace SharedGameData.ExtensionMethods
+{
+    public static class PointExtensions
+    {
+        public static Vector2 ToVector2(this Point p)
+        {
+            return new Vector2(p.X, p.Y);
+        }
+
+        public static Microsoft.Xna.Framework.Point ToXnaPoint(this Point p)
+        {
+            return new Microsoft.Xna.Framework.Point(p.X, p.Y);
+        }
+    }
+}
---
+++ SharedGameData/ExtensionMethods/RectangleExtensions.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using Microsoft.Xna.Framework;
+using Point = System.Drawing.Point;
+
+#endregion
+
+namespace SharedGameData.ExtensionMethods
+{
+    public static class RectangleExtensions
+    {
+        public static bool Contains(this Rectangle rect, Point p)
+        {
+            return rect.Contains(p.ToXnaPoint());
+        }
+
+        public static Vector2[] GetCorners(this Rectangle rect)
+        {
+            return new Vector2[]
+            {
+                // Top Left
+                new Vector2(rect.X, rect.Y),
+
+                // Top Right
+                new Vector2(rect.X + rect.Width, rect.Y),
+
+                // Bottom Left
+                new Vector2(rect.X, rect.Y + rect.Height),
+
+                // Bottom Right
+                new Vector2(rect.X + rect.Width, rect.Y + rect.Height)
+            };
+        }
+    }
+}
